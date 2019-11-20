bfb7adb497c37f5a5ed1c9d02150d5f12aa0f69c
mashjprime@gmail.com <*!4NM$%gDY@t#48R>
2019-11-20 21:20:28 +0800

    Initial Commit - Seconds countdown

---
+++ BillionSecond/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BillionSecond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.Clear();
+                Console.WriteLine(countDown());
+                Console.ReadLine(); 
+             }
+
+        }
+
+        public static double countDown()
+        {
+            TimeSpan span = DateTime.Now.Subtract(new DateTime(2020, 1, 1, 0, 0, 0));
+            return span.TotalSeconds;
+        }
+
+    }
+}
