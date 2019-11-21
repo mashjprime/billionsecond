08e2b01f22ad115956f38f756e3d29e666249eb2
Mash J Prime <mashjprime@gmail.com>
2019-11-21 20:27:40 +0800

    Basic Billion Second addition logic complete

--- BillionSecond/Program.cs
+++ BillionSecond/Program.cs
@@ -6,18 +6,45 @@ namespace BillionSecond
     {
         static void Main(string[] args)
         {
-            while (true) {
+            int birthYear;
+            int birthMonth;
+            int birthDay;
+
+            Console.WriteLine("Please enter your birth year:");
+            int.TryParse(Console.ReadLine(), out birthYear);
+            Console.WriteLine("Please enter your birth month:");
+            int.TryParse(Console.ReadLine(), out birthMonth);
+            Console.WriteLine("Please enter day of month:");
+            int.TryParse(Console.ReadLine(), out birthDay);
+
+            DateTime bd = new DateTime(birthYear, birthMonth, birthDay, 0, 0, 0);
+            DateTime billSec = bd.AddSeconds(1000000000);
+
+
+            TimeSpan span = DateTime.Now.Subtract(billSec);
+            Console.WriteLine(span.TotalSeconds);
+            Console.WriteLine("Birthday:        " + bd);
+            Console.WriteLine("Billion Seconds: " + billSec);
+
+           
+
+
+            Console.ReadLine();
+
+           /* while (true) {
                 Console.Clear();
                 Console.WriteLine(countDown());
+                Console.WriteLine(DateTime.Now.ToString("h:mmtt d/MM/yyyy"));
                 Console.ReadLine(); 
-             }
+             }*/
 
         }
 
         public static double countDown()
         {
-            TimeSpan span = DateTime.Now.Subtract(new DateTime(2020, 1, 1, 0, 0, 0));
-            return span.TotalSeconds;
+            // TimeSpan span = DateTime.Now.Subtract(new DateTime(2020, 1, 1, 0, 0, 0));
+            // return span.TotalSeconds;
+            return 0.0;
         }
 
     }
